927270f5e94217508a5f1d9c1d19db57f8b570f8
Christopher Wells <cwellsny@nycap.rr.com>
2016-11-22 13:58:39 -0500

    Implement the interpreter
    Implement the interpreter for the language.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+import System.Console.Haskeline
+import Text.ParserCombinators.Parsec (parse)
+
+import Interpreter.TinyTinyLisp.Parser
 
 main :: IO ()
-main = undefined
+main = runInputT defaultSettings loop
+
+loop :: InputT IO ()
+loop = do
+  minput <- getInputLine "% "
+  case minput of
+    Nothing    -> return ()
+    Just ":q"  -> return ()
+    Just input -> do
+      outputStrLn $ processLine input
+      loop
+
+processLine :: String -> String
+processLine line = case parse valueParser "" line of
+  Right val -> show val
+  Left  err -> show err
